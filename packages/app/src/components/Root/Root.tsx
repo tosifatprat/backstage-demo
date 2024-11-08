34aed29edf2e71124e259d65ddfa298050a45c4d
Tosif khan <tosif.khan@pratishthanventures.com>
2024-11-08 18:04:40 +0530

    fixed sidebar: create catalog route

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -76,7 +76,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
-        <SidebarItem icon={AddCircleOutlineIcon} to="create-catalog" text="Create Catalog..." />
+        <SidebarItem icon={AddCircleOutlineIcon} to="catalog-import" text="Create Catalog..." />
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
